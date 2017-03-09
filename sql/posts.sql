73a3db9b27b2784a33ad4aa97014e881ff38eb8d
ironic <ronjhagen@gmail.com>
2017-03-09 10:13:13 -0600

    create post related tables and insert test data

--- sql/posts.sql
+++ sql/posts.sql
@@ -1,25 +1,72 @@
---Posts
+-- usage: msyql -u [username] -p [dbname] < posts.sql
+
+drop table if exists created_posts;
+drop table if exists followed_posts;
+drop table if exists posts;
+drop table if exists users;
+
+-- copy the users ddl from tablecreation so we can run one script
+CREATE TABLE users (
+	user_id		INT(11) NOT NULL AUTO_INCREMENT, 	
+	user_name 	VARCHAR(25) NOT NULL,				
+	first_name 	VARCHAR(50) NOT NULL,				
+	last_name 	VARCHAR(50) NOT NULL,				
+	user_pass 	VARCHAR(25) NOT NULL,				
+	user_email	VARCHAR(50) NOT NULL,				
+	user_zip 	INT(5) NOT NULL,				
+	user_level 	INT(1) DEFAULT 1, 					
+	create_dt 	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE INDEX user_name_unique (user_name),
+	PRIMARY KEY (user_id)
+)	ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Posts
 CREATE TABLE posts (
 	post_id		INT(11) NOT NULL AUTO_INCREMENT, 	
 	post_date 	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	content 	TEXT NOT NULL,
-	PRIMARY KEY (post_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	user_id int not null,
+	PRIMARY KEY (post_id),
+	foreign key (user_id) references users(user_id)
+) 	ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---Followed posts
+-- Followed posts
 CREATE TABLE followed_posts (
 	user_id INT(11) NOT NULL,
 	post_id INT(11) NOT NULL,
 	PRIMARY KEY (user_id, post_id),
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
-	FOREIGN KEY (post_id) REFERENCES posts(post_id),	
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	FOREIGN KEY (post_id) REFERENCES posts(post_id)	
+) 	ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---Created posts
+-- Created posts
+-- I don't think we need this table, userid should be attribute of posts
+-- a user can have many posts, but a post can have only one 'creator'
 CREATE TABLE created_posts (
 	user_id INT(11) NOT NULL,
 	post_id INT(11) NOT NULL,
 	PRIMARY KEY (user_id, post_id),
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
-	FOREIGN KEY (post_id) REFERENCES posts(post_id),	
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	FOREIGN KEY (post_id) REFERENCES posts(post_id)	
+) 	ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- i think there may be an issue with using an int for zip code. this one is for boston
+insert into users (user_name, first_name, last_name, user_pass, user_email, user_zip)
+values ('test1', 'test', 'one', 'password', 'test1@test.com', 01841);
+
+insert into users (user_name, first_name, last_name, user_pass, user_email, user_zip)
+values ('test2', 'test', 'two', 'password', 'test2@test.com', 90210);
+
+insert into posts (content, user_id)
+values ('my first post', (select user_id from users where user_name = 'test1'));
+
+insert into posts (content, user_id)
+values ('my second post has more content than my first', (select user_id from users where user_name = 'test1'));
+
+insert into posts (content, user_id)
+values ('this is a post from the second user', (select user_id from users where user_name = 'test2'));
+
+insert into followed_posts (user_id, post_id)
+values ((select user_id from users where user_name = 'test2'),
+	(select post_id from posts where content = 'my first post'));
+
