e8dac528f3d744fec52f742fc5f48699978bdaa7
jredp <parkinja@oregonstate.edu>
2017-03-08 18:34:52 -0700

    Sql fix

--- sql/TableCreation.sql
+++ sql/TableCreation.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
 	last_name 	VARCHAR(50) NOT NULL,				
 	user_pass 	VARCHAR(25) NOT NULL,				
 	user_email	VARCHAR(50) NOT NULL,				
-	user_zip 	VARCHAR(10) NOT NULL,				
+	user_zip 	INT(5) NOT NULL,				
 	user_level 	INT(1) DEFAULT 1, 					
 	create_dt 	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	UNIQUE INDEX user_name_unique (user_name),
