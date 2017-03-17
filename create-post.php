19ffa8c42a510962f405818a21cfd9e821de9818
ironic <ronjhagen@gmail.com>
2017-03-17 16:28:06 -0500

    add image resize to add, edit post

--- create-post.php
+++ create-post.php
@@ -7,7 +7,11 @@ if (!isset($_SESSION['user_name'])) {
 	echo "<script>window.location = 'index.php?loggedin=no'</script>";
 }
 
+// TODO: major refactor!!
+// lots of help from this post on the tricky parts
+// http://stackoverflow.com/questions/18805497/php-resize-image-on-upload
 if (isset($_POST['submit'])) {
+	// if this is still zero-length at the end we're good
 	$status = '';
 	if (!isset($_POST['content']) || $_POST['content'] == '')
 		$status = 'post content is required';
@@ -16,24 +20,63 @@ if (isset($_POST['submit'])) {
 	$valid_files = $mypost->getValidFileTypes();
 	// just a guess, didn't do a lot of research on bytes -> mb conversion
 	$max_size = 1024000;
+	$max_dim = 400;
+	// if there is an image
 	if ($_FILES['post_image']['error'] == 0) {
 
+		// get the dims
+		list($width, $height, $type, $attr) = getimagesize($_FILES['post_image']['tmp_name']);
+		// only need this to get the extension
 		$filename = $upload_dir . $_FILES['post_image']['name'];
-		$img_type = '.' . pathinfo($filename, PATHINFO_EXTENSION);
+		$img_type = '.' . strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 
-		if (!in_array(strtolower($img_type), $valid_files)) {
+		$target_filename = $_FILES['post_image']['tmp_name'];
+		$raw_file = $_FILES['post_image']['tmp_name'];
+
+		// make sure it's valid and not too big
+		$too_big = false;
+		if (!in_array($img_type, $valid_files)) {
 			$status = 'invalid image filetype';
 		} else if ($_FILES['post_image']['size'] > $max_size) {
 			$status = 'image file over 1 Mb limit';
 		} else {
-			// assign unique name to image
-			$filename = $_SESSION['user_name'] . '_' . date("YmdHis") . $img_type;
-			if (move_uploaded_file($_FILES['post_image']['tmp_name'], $upload_dir . $filename)) {
-			} else {
-				$status = 'error uploading file';
+			// resize image if too large
+			if ($width > $max_dim || $height > $max_dim) {
+				// make sure we resize it
+				$too_big = true;
+				$size = getimagesize($raw_file);
+				$ratio = $size[0] / $size[1];
+				if ($ratio > 1) {
+					$width = $max_dim;
+					$height = $max_dim / $ratio;
+				} else {
+					$width = $max_dim * $ratio;
+					$height = $max_dim;
+				}
 			}
 		}
 
+		// do this only if file too large
+		if ($too_big) {
+			$filename = $_SESSION['user_name'] . '_' . date("YmdHis") . $img_type;
+			$src = imagecreatefromstring(file_get_contents($raw_file));
+			$dst = imagecreatetruecolor($width, $height);
+			imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $size[0], $size[1]);
+			imagedestroy($src);
+
+			if ($img_type == 'png')
+				imagepng($dst, $target_filename);
+			else if ($img_type == 'gif')
+				imagegif($dst, $target_filename);
+			else
+				imagejpeg($dst, $target_filename);
+			imagedestroy($dst);
+		}
+
+		if (move_uploaded_file($target_filename, $upload_dir . $filename)) {
+		} else {
+			$status = 'error uploading file';
+		}
 	}
 
 	if ($status == '') {
--- edit-post.php
+++ edit-post.php
@@ -8,6 +8,9 @@ if (!isset($_SESSION['user_name'])) {
 	exit;
 }
 
+// TODO: major refactor!!
+// lots of help from this post on the tricky parts
+// http://stackoverflow.com/questions/18805497/php-resize-image-on-upload
 if (isset($_POST['submit'])) {
 	$status = '';
 	if (!isset($_POST['conent']) && $_POST['content'] == '')
@@ -18,24 +21,62 @@ if (isset($_POST['submit'])) {
 	$valid_files = $mypost->getValidFileTypes();
 	// just a guess, didn't do a lot of research on bytes -> mb conversion
 	$max_size = 1024000;
+	$max_dim = 400;
+	// if there is an image
 	if ($_FILES['post_image']['error'] == 0) {
-
+		// get the dims
+		list($width, $height, $type, $attr) = getimagesize($_FILES['post_image']['tmp_name']);
+		// only need this to get the extension
 		$filename = $upload_dir . $_FILES['post_image']['name'];
-		$img_type = '.' . pathinfo($filename, PATHINFO_EXTENSION);
+		$img_type = '.' . strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 
-		if (!in_array(strtolower($img_type), $valid_files)) {
+		$target_filename = $_FILES['post_image']['tmp_name'];
+		$raw_file = $_FILES['post_image']['tmp_name'];
+
+		// make sure it's valid and not too big
+		$too_big = false;
+		if (!in_array($img_type, $valid_files)) {
 			$status = 'invalid image filetype';
 		} else if ($_FILES['post_image']['size'] > $max_size) {
 			$status = 'image file over 1 Mb limit';
 		} else {
-			// assign unique name to image
-			$filename = $_SESSION['user_name'] . '_' . date("YmdHis") . $img_type;
-			if (move_uploaded_file($_FILES['post_image']['tmp_name'], $upload_dir . $filename)) {
-			} else {
-				$status = 'error uploading file';
+			// resize image if too large
+			if ($width > $max_dim || $height > $max_dim) {
+				// make sure we resize it
+				$too_big = true;
+				$size = getimagesize($raw_file);
+				$ratio = $size[0] / $size[1];
+				if ($ratio > 1) {
+					$width = $max_dim;
+					$height = $max_dim / $ratio;
+				} else {
+					$width = $max_dim * $ratio;
+					$height = $max_dim;
+				}
 			}
 		}
 
+		// do this only if file too large
+		if ($too_big) {
+			$filename = $_SESSION['user_name'] . '_' . date("YmdHis") . $img_type;
+			$src = imagecreatefromstring(file_get_contents($raw_file));
+			$dst = imagecreatetruecolor($width, $height);
+			imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $size[0], $size[1]);
+			imagedestroy($src);
+
+			if ($img_type == 'png')
+				imagepng($dst, $target_filename);
+			else if ($img_type == 'gif')
+				imagegif($dst, $target_filename);
+			else
+				imagejpeg($dst, $target_filename);
+			imagedestroy($dst);
+		}
+
+		if (move_uploaded_file($target_filename, $upload_dir . $filename)) {
+		} else {
+			$status = 'error uploading file';
+		}
 	}
 
 	if ($status == '') {
@@ -72,11 +113,11 @@ if (isset($_POST['submit'])) {
 			<a href="logout.php">logout</a>
 		</p>
 
-		<?php
-			// populate the form
-			if (isset($_GET['pid']))
-				extract($mypost->getId($_GET['pid']));
-		?>
+<?php
+// populate the form
+if (isset($_GET['pid']))
+	extract($mypost->getId($_GET['pid']));
+?>
 
 		<form enctype="multipart/form-data" method="post">
 		<table>
@@ -85,14 +126,14 @@ if (isset($_POST['submit'])) {
 			</tr>
 			<tr>
 				<td>current post image: </td>
-				<?php
-					if (!is_null($post_img) && strlen(trim($post_img)) > 0) {
-						echo '<td><img src="' . $mypost->getImgDir() . $post_img . '"></td>';
-						echo '<input type="hidden" name="old_image" value="' . $post_img . '">';
-					} else {
-						echo '<td>&nbsp;</td>';
-					}
-				?>
+<?php
+if (!is_null($post_img) && strlen(trim($post_img)) > 0) {
+	echo '<td><img src="' . $mypost->getImgDir() . $post_img . '"></td>';
+	echo '<input type="hidden" name="old_image" value="' . $post_img . '">';
+} else {
+	echo '<td>&nbsp;</td>';
+}
+?>
 			<tr>
 				<td>upload new image:</td>
 				<td><input type="file" name="post_image"></td>
